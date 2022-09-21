64269f214975cb48054f29a04730b3f6b8625b9d
Xander <167364@student.vansd.org>
2022-09-21 10:41:23 -0700

    Added full player control over the bus

--- createwithcode/prototype 1/Assets/Scripts/PlayerController.cs
+++ createwithcode/prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
     public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
      // Move the vehicle forward   
-     transform.Translate(Vector3.forward * Time.deltaTime * speed);
-    transform.Translate(Vector3.right * Time.deltaTime * turnSpeed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 
 }
