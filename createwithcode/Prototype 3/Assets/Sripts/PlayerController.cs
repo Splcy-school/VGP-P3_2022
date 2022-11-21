7258aa6b91e7832213ee61a30a4068e2307c1897
Xander <167364@student.vansd.org>
2022-11-21 10:14:13 -0800

    added obstacles and background moveing

--- createwithcode/Prototype 3/Assets/Sripts/PlayerController.cs
+++ createwithcode/Prototype 3/Assets/Sripts/PlayerController.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpforce = 7;
+    public float jumpforce = 40;
     public float gravityModifier;
+    public bool isOnGround = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             playerRb.AddForce(Vector3.up *  jumpforce, ForceMode.Impulse);
+            isOnGround = false;
         }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
